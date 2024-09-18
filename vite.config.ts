6422491d358edff9395b7a8fb846c567b4f76fc9
Artemida <maxartem0419@gmail.com>
2024-09-18 17:08:10 +0300

    add zip package

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import Inspect from 'vite-plugin-inspect'
 import tsconfigPaths from 'vite-tsconfig-paths';
+import zipPack from "vite-plugin-zip-pack";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tsconfigPaths(), react(), Inspect()],
+  plugins: [tsconfigPaths(), react(), Inspect(), zipPack()],
 
   server: {
     port: 6600,
